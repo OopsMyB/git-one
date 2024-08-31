33a9f69eb0b24f63ff9e736a1633f8345ce4021f
OopsMyB <ranjanrn@mail.uc.edu>
2024-08-30 22:10:06 -0400

    Add files via upload

--- git-one.cpp
+++ git-one.cpp
@@ -1,17 +1,35 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-
 int main() {
+    int total = 0;  // Initialize total to 0
     string x;
-
+    string y;
 
     cout << "Hello World!" << endl;
     cout << "I need another commit :( " << endl;
-    cin >> x;
-    if (x == "womp womp");
-        cout << "womp womp? more like yippe!" << endl;
     
+    while (true) {  // BROKEN
+        cin >> x;
+        cout << total << endl;
+        
+        if (x == "womp womp") {
+            cout << "womp womp? more like yippe!" << endl;
+            break;
+        } else {
+            cout << "say womp womp" << endl;
+            total += 1;
+        }
+    }
 
+    cin >> y;
+    if (y == "nah") {
+        cout << "you right" << endl;
+    } else {
+        cout << "say nah" << endl;
+    }
+
+    return 0;
 }
